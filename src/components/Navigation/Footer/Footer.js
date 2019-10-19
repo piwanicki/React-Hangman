51521a1e69b97538dacb2cce43f13ba429f97b15
piwanicki <pawel.iwanicki92@gmail.com>
2019-10-19 18:11:58 +0200

    add some styling and icons to footer nav item s

--- src/components/Navigation/Footer/Footer.js
+++ src/components/Navigation/Footer/Footer.js
@@ -1,11 +1,28 @@
 import React from 'react';
 import classes from './Footer.module.css';
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCodeBranch, faEnvelope, faHome, faTimesCircle } from '@fortawesome/free-solid-svg-icons';
+import hangmanLogo from '../../../assets/img/hangmanIconColor.png';
+import ticTacToeLogo from '../../../assets/img/tictactoe-icon2.png'
 
 const Footer = (props) => {
+
   return (
     <footer className={classes.Footer}>
-      <p>Footer</p>
+      <div className={classes.InfoSection}>
+        <ul>
+          <li><FontAwesomeIcon icon={faCodeBranch}/> Github</li>
+          <li><FontAwesomeIcon icon={faEnvelope}/> Contact Me</li>
+        </ul>
+      </div>
+      <div className={classes.QuickNav}>
+        <ul>
+          <li><FontAwesomeIcon icon={faHome}/> Home</li>
+          <li><img src={ticTacToeLogo} alt="tictactoe logo" /> TicTacToe</li>
+          <li><img src={hangmanLogo} alt="hangman logo" />Hangman</li>
+        </ul>
+      </div>
+    
     </footer>
   )
 }
--- src/containers/Layout/Layout.js
+++ src/containers/Layout/Layout.js
@@ -41,6 +41,7 @@ class Layout extends Component {
            <main className={classes.Content}>
              {this.props.children}
            </main>
+    
         </div>
         <Footer /> 
       </Auxiliary>
