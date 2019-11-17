0bfa53a460c67f5396b98f958e9bb5cf1b32dab2
Pawel Iwanicki <pawel.iwanicki92@gmail.com>
2019-11-17 19:29:55 +0100

    some styling changes in Highscore module

--- src/components/Highscore/Highscore.js
+++ src/components/Highscore/Highscore.js
@@ -10,15 +10,16 @@ class HighScore extends Component {
 
     const scores = this.props.score; 
     let scoreArr = [];
-    
+
     for( const el of scores) {
-      // console.log(`${el[0]} &#45 ${el[1]}`)
-      scoreArr.push(`${el[0]}  ${el[1]} Pts`);
+      scoreArr.push(`${el[0]}  ${el[1]}.Pts`);
     }
+
+
 
     return (
       <div className={classes.HighScore}>
-        <p style={{fontSize:'1.2em'}}><FontAwesomeIcon icon={faTrophy} className={classes.FirstPlace}/> {scoreArr.length > 0 ? scoreArr[0] : null} </p>
+        <p style={{fontSize:'1.2em'}}><FontAwesomeIcon icon={faTrophy} className={classes.FirstPlace}/> {scoreArr.length > 0 ? scoreArr[0] : null}</p>
         <p style={{fontSize:'1em'}}><FontAwesomeIcon icon={faTrophy} className={classes.SecondPlace}/> {scoreArr.length > 1 ? scoreArr[1] : null}</p>
         <p style={{fontSize:'0.75em'}}><FontAwesomeIcon icon={faTrophy} className={classes.ThirdPlace}/> {scoreArr.length > 2 ? scoreArr[2] : null}</p>
       </div>
